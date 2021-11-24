f92b44e9d8b8769f6e31968b818e3d95e537a26e
sojinkim <60996449+programmerGin@users.noreply.github.com>
2021-11-24 19:32:51 +0900

    Chat Server and Client with C#
    just a basic chat Server with Client. it was an assignment in uni

---
+++ ChatClient/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream;
+        string readData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Connect
+        {
+            clientSocket.Connect("127.0.0.1", 7000); //서버 포트번호
+            readData = "Connect To server...";
+            msg();
+
+            serverStream = clientSocket.GetStream();
+            byte[] outBytes = Encoding.ASCII.GetBytes(textBox1.Text + "$");
+            serverStream.Write(outBytes, 0, outBytes.Length);
+            serverStream.Flush();
+
+            Thread clThread = new Thread(getMessage);
+            clThread.Start();
+        }
+
+        private void getMessage()
+        {
+            byte[] inBytes = new byte[clientSocket.ReceiveBufferSize];
+            string returnData;
+            while(true)
+            {
+                
+                serverStream.Read(inBytes, 0, clientSocket.ReceiveBufferSize);
+                returnData = Encoding.ASCII.GetString(inBytes);
+               // readData = returnData.Substring(0, returnData.IndexOf("$"));
+                readData = " " + returnData;
+                msg();
+            }
+        }
+        private void msg()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+                textBox2.Text += Environment.NewLine + "<<" + readData;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Send
+        {
+            byte[] outBytes = Encoding.ASCII.GetBytes(textBox3.Text + "$");
+            serverStream.Write(outBytes, 0, outBytes.Length);
+            serverStream.Flush();
+
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class Program
+    {
+        public static Hashtable clientList = new Hashtable();
+        static void Main(string[] args)
+        {
+          TcpListener serverSocket =   new TcpListener(7000);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine("Chat Server Started....");
+
+            while(true)
+            {
+                Console.WriteLine("Chat Server Waiting....");
+
+                clientSocket =  serverSocket.AcceptTcpClient();
+                counter++;
+
+                byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+                string dataFromClient = null;
+
+                NetworkStream networkStream =  clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+                dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientList.Add(dataFromClient, clientSocket);
+                Console.WriteLine(dataFromClient + " Joined ");
+
+                broadcast(dataFromClient + " Joined", dataFromClient, false);
+
+                handleClient client =  new handleClient();
+                client.startClient(clientSocket, dataFromClient, clientList);
+
+            
+            }
+
+        }
+
+        private static void broadcast(string msg, string uName, bool flag)
+        {
+            TcpClient broadcastSocket;
+            NetworkStream broadcastStream;
+            byte[] broadcastBytes = null;
+            foreach (DictionaryEntry item in clientList)
+            {
+                 broadcastSocket = (TcpClient)item.Value;
+                 broadcastStream =  broadcastSocket.GetStream();
+                if (flag)
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says :" +msg);
+                else 
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+
+        private class handleClient
+        {
+            TcpClient clientSocket;
+            string clNo;
+            Hashtable clientList;
+
+
+            public handleClient()
+            {
+            }
+
+            public void startClient(TcpClient clientSocket, string dataFromClient, Hashtable clientList)
+            {
+                this.clientSocket = clientSocket;
+                this.clNo = dataFromClient;
+                this.clientList = clientList;
+
+               Thread clThread =  new Thread(doChat);
+                clThread.Start();
+            }
+
+            private void doChat()
+            {
+                byte[] byteFrom = new byte[clientSocket.ReceiveBufferSize];
+                string dataFromClient = null;
+                NetworkStream networkStream;
+                while(true)
+                {
+                    try
+                    {
+                        networkStream = clientSocket.GetStream();
+                        networkStream.Read(byteFrom, 0, clientSocket.ReceiveBufferSize);
+                        dataFromClient = Encoding.ASCII.GetString(byteFrom);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                        Console.WriteLine("From CLient - ", clNo, " : " + dataFromClient);
+
+                        Program.broadcast(dataFromClient, clNo, true);
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+
+            }
+        }
+    }
+}
